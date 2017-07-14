c18eb3f7e82ad1d73a856d5d8ba92d6ff3b0b504
David Ringsdorf <git@drdf.de>
2017-07-14 22:52:34 +0200

    added: html.php and .htaccess

---
+++ html.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * @author David Ringsdorf http://davidringsdorf.de
+ * @license MIT License
+ */
+require __DIR__ . '/genug_core/genug/bootstrap.php';
+
+(function () {
+    try {
+        \ob_start();
+        \http_response_code(200);
+        try {
+            genug\Api::requestedPage();
+            require_once genug\Setting\TEMPLATE_DIR . '/main.html.php';
+        } catch (\genug\throwable_RequestedPageNotFound $t) {
+            \ob_clean();
+            \http_response_code(404);
+            require_once genug\Setting\TEMPLATE_DIR . '/error404.html.php';
+        }
+    } catch (\Throwable $t) {
+        \ob_clean();
+        \http_response_code(500);
+        echo '500 Internal Server Error';
+        throw $t;
+    } finally {
+        \ob_end_flush();
+    }
+})();
