2eee15bb7d55c3c93a48cee48b1ec4f9d48601d7
David Ringsdorf <git@drdf.de>
2017-07-15 16:00:07 +0200

    added: demo template

---
+++ genug_user/template/error404.html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Not Found (404)</title>
+</head>
+<body>
+  <h1>Not Found (404)</h1>
+  <nav>
+    <a
+      href="<?= genug\Api\URL_PATH_BASE . genug\Api::homepage()->id() ?>">homepage</a>
+  </nav>
+</body>
+</html>
---
+++ genug_user/template/main.html.php
@@ -0,0 +1,49 @@
+<?php
+use genug\Api as g;
+use const genug\Api\URL_PATH_BASE;
+
+const SITE_TITLE = 'my site';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?= g::requestedPage()->title()?><?php if (g::requestedPage()->category() !== g::mainCategory()) echo ' - ' . g::requestedPage()->category()->title() ?> | <?= SITE_TITLE ?></title>
+<link
+  rel="stylesheet"
+  href="<?= URL_PATH_BASE ?>/asset/css/style.css" />
+</head>
+<body>
+  <header>
+    <h1>
+      <a href="<?= URL_PATH_BASE . g::homepage()->id() ?>"><?= SITE_TITLE ?></a>
+    </h1>
+  </header>
+  <main>
+  <article>
+    <header>
+      <h1><?= g::requestedPage()->title() ?></h1>
+    </header>
+    <?= g::requestedPage()->content()?>
+    <aside>
+      <ul>
+        <li>date: <time
+            datetime="<?= g::requestedPage()->date()->format('c') ?>"><?= g::requestedPage()->date()->format(DATE_RFC1123) ?></time></li>
+        <li>category: <span
+          data-category="<?= g::requestedPage()->category()->id() ?>"><?= g::requestedPage()->category()->title() ?></span></li>
+      </ul>
+    </aside>
+  </article>
+  </main>
+  <nav>
+    <h1>all pages</h1>
+    <ul>
+  <?php foreach (g::pages() as $page): ?>
+  <li><a
+        href="<?= URL_PATH_BASE . $page->id() ?>"
+        class="<?php if ($page === g::requestedPage()) echo 'is_requested' ?>"><?= $page->title() ?></a></li>
+  <?php endforeach; ?>
+  </ul>
+  </nav>
+</body>
+</html>
