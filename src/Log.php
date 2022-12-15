973081d24543e13ccf7cd1aad74ddb437749a18b
David Schwarz <mail+git@davidschwarz.eu>
2022-12-15 08:58:47 +0100

    added: genug\Log

---
+++ src/Log.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace genug;
+
+use Psr\Log\LoggerInterface;
+use genug\Environment\Preset;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger as MonologLogger;
+
+/**
+ *
+ * @author David Ringsdorf http://davidringsdorf.de
+ * @license MIT License
+ */
+final class Log
+{
+    protected static array $instances = [];
+
+    public static function instance(string $name): LoggerInterface&MonologLogger
+    {
+        if (! isset(static::$instances[$name])) {
+            static::$instances[$name] = static::instantiateLogger($name);
+        }
+        return static::$instances[$name];
+    }
+
+    protected function __construct(){}
+
+    protected static function instantiateLogger(string $name): MonologLogger
+    {
+        $debugEnvValueString = getenv('GENUG_DEBUG', true) ?: Preset::GENUG_DEBUG->value;
+        $isDebugOrNullOnFailure = filter_var($debugEnvValueString, FILTER_VALIDATE_BOOL, FILTER_NULL_ON_FAILURE);
+        
+        $debugLogFilePath = getenv('GENUG_DEBUG_LOGFILE', true) ?: Preset::GENUG_DEBUG_LOGFILE->value;
+
+        $logger = new MonologLogger($name);
+        $level = Level::Warning;
+        if ((bool) $isDebugOrNullOnFailure) {
+            $level = Level::Debug;
+        }
+        $logger->pushHandler(new StreamHandler('php://stderr', $level));
+
+        if ((bool) $isDebugOrNullOnFailure) {
+            $logger->pushHandler(new StreamHandler($debugLogFilePath , Level::Debug));
+        }
+        return $logger;
+    }
+}
