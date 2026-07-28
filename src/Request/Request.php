5fce169a84e009797e44095900cb432f3e87d305
David J. Schwarz <mail+git@davidschwarz.eu>
2026-07-28 16:34:29 +0200

    change: Request pathBase is now configuratable

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -15,8 +15,10 @@ namespace genug\Request;
 
 use RuntimeException;
 
-use function dirname;
 use function parse_url;
+use function sprintf;
+use function str_ends_with;
+use function str_starts_with;
 
 use const PHP_URL_PATH;
 
@@ -28,22 +30,22 @@ use const PHP_URL_PATH;
 final class Request implements RequestInterface
 {
     public function __construct(
+        private readonly string $pathBase = ''
     ) {
+        if (str_ends_with($this->pathBase, '/')) {
+            throw new RuntimeException(sprintf('Path base "%s" must not end with a slash.', $this->pathBase));
+        }
     }
 
     public function pageId(): string
     {
         $_requestUri = $_SERVER['REQUEST_URI'] ?? throw new RuntimeException();
         $path = parse_url($_requestUri, PHP_URL_PATH);
-        $pathBase = (function (): string {
-            $_scriptName = $_SERVER['SCRIPT_NAME'] ?? throw new RuntimeException();
-            $pathBase = dirname($_scriptName);
-            if (\strlen($pathBase) === 1) {
-                // $path_base is '\' (windows) OR '/' (linux) OR '.'
-                $pathBase = '';
-            }
-            return $pathBase;
-        })();
-        return substr($path, strlen($pathBase));
+
+        if (! str_starts_with($path, $this->pathBase)) {
+            throw new RuntimeException(sprintf('Request URI "%s" does not start with path base "%s".', $path, $this->pathBase));
+        }
+
+        return substr($path, strlen($this->pathBase));
     }
 }
