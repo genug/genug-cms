ff9ddb8bb1978841d01ea595c33f15befb7f4ce3
David J. Schwarz <mail+git@davidschwarz.eu>
2026-07-29 00:50:22 +0200

    refactor Logger

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -15,7 +15,7 @@ namespace genug\Logger;
 
 use Override;
 use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
+use Psr\Log\LoggerTrait;
 use Stringable;
 
 /**
@@ -25,57 +25,11 @@ use Stringable;
  */
 final class Logger implements LoggerInterface
 {
+    use LoggerTrait;
+
     public function __construct(
         private LoggerInterface $internalLogger
     ) {
-    }
-
-    #[Override]
-    public function emergency(string|Stringable $message, array $context = []): void
-    {
-        $this->log(LogLevel::EMERGENCY, $message, $context);
-    }
-
-    #[Override]
-    public function alert(string|Stringable $message, array $context = []): void
-    {
-        $this->log(LogLevel::ALERT, $message, $context);
-    }
-
-    #[Override]
-    public function critical(string|Stringable $message, array $context = []): void
-    {
-        $this->log(LogLevel::CRITICAL, $message, $context);
-    }
-
-    #[Override]
-    public function error(string|Stringable $message, array $context = []): void
-    {
-        $this->log(LogLevel::ERROR, $message, $context);
-    }
-
-    #[Override]
-    public function warning(string|Stringable $message, array $context = []): void
-    {
-        $this->log(LogLevel::WARNING, $message, $context);
-    }
-
-    #[Override]
-    public function notice(string|Stringable $message, array $context = []): void
-    {
-        $this->log(LogLevel::NOTICE, $message, $context);
-    }
-
-    #[Override]
-    public function info(string|Stringable $message, array $context = []): void
-    {
-        $this->log(LogLevel::INFO, $message, $context);
-    }
-
-    #[Override]
-    public function debug(string|Stringable $message, array $context = []): void
-    {
-        $this->log(LogLevel::DEBUG, $message, $context);
     }
 
     #[Override]
