6b7f8c65e1220796d095bb779219e8b35812deee
David J. Schwarz <mail+git@davidschwarz.eu>
2026-08-02 03:54:32 +0200

    changed view (WIP)

--- genug_user/view/index.php
+++ genug_user/view/index.php
@@ -6,29 +6,8 @@
     <link rel="stylesheet" href="/asset/css/style.css" />
 </head>
 <body>
-    <h1><?= $genug->requestedPage->title ?></h1>
-    <p><time datetime="<?= $genug->requestedPage->dateTime ?>"><?= $genug->requestedPage->dateTime?->format(DATE_RFC1123) ?></time></p>
-    <ul>
-        <li>PageGroup: <?= $genug->requestedPage->group; ?></li>
-        <?php $currentGroup = $genug->groups->fetchOrNull($genug->requestedPage->group); ?>
-        <li>Group ID: <?= $currentGroup?->id ?></li>
-        <li>Group Title: <?= $currentGroup?->title ?></li>
-    </ul>
-    <?= $genug->requestedPage->content ?>
-
-    <nav>
-        <h1>all pages</h1>
-        <ul>
-<?php foreach ($genug->pages as $page): ?>
-    <?php if (! $page->id->equals($genug->setting->notFoundPageId)): ?>
-            <li>
-                <a href="<?= $page->id ?>"<?php if ($page->equals($genug->requestedPage)) {
-                    echo ' aria-current="page"';
-                } ?>><?= $page->title ?? $page->id ?></a>
-            </li>
-    <?php endif; ?>
-<?php endforeach; ?>
-        </ul>
-    </nav>
+<pre>
+<?= $genug->requestedPage->content ?>
+</pre>
 </body>
 </html>
