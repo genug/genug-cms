75c812e35f6e456e95de7d783919859f1fe95c42
David J. Schwarz <mail+git@davidschwarz.eu>
2026-08-02 02:49:44 +0200

    add EquatableSimpleObjectTrait

---
+++ src/Lib/EquatableSimpleObjectTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of "genug".
+ *
+ * (c) David J. Schwarz
+ * https://davidschwarz.eu
+ *
+ * License: MIT License
+ */
+
+namespace genug\Lib;
+
+use function is_object;
+
+/**
+ *
+ * @author David J. Schwarz <https://davidschwarz.eu/>
+ * @license MIT License
+ */
+
+trait EquatableSimpleObjectTrait
+{
+    final public function equals(mixed $other): bool
+    {
+        if (! is_object($other)) {
+            return false;
+        }
+
+        return ($this == $other);
+    }
+}
