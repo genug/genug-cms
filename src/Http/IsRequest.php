8d7ae4087d04f4c4100b6b2ec44f8a7d741ef22c
David J. Schwarz <mail+git@davidschwarz.eu>
2026-08-06 23:40:46 +0200

    add IsRequest

---
+++ src/Http/IsRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of "genug".
+ *
+ * (c) David J. Schwarz
+ * https://davidschwarz.eu
+ *
+ * License: MIT License
+ */
+
+namespace genug\Http;
+
+use Exception;
+use Uri\Rfc3986\Uri;
+
+trait IsRequest
+{
+    public protected(set) Method $method;
+    public protected(set) string $path;
+
+    public function __construct(
+        Method $method,
+        Uri $uri,
+    ) {
+        $this->method = $method;
+        $this->path = $uri->getPath();
+    }
+
+    public function accepts(ContentType ...$contentType): bool
+    {
+        throw new Exception('Not implemented');
+    }
+}
