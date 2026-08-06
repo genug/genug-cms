3bbc611786d6ae0fba20b5c5eb56b3e2c973061a
David J. Schwarz <mail+git@davidschwarz.eu>
2026-08-06 23:25:11 +0200

    add contenttype enum

---
+++ src/Http/ContentType.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of "genug".
+ *
+ * (c) David J. Schwarz
+ * https://davidschwarz.eu
+ *
+ * License: MIT License
+ */
+
+namespace genug\Http;
+
+enum ContentType: string
+{
+    case HTML = 'text/html';
+}
