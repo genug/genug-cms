7d83582f1522a3d1934f445b3f9be191fdeef711
David J. Schwarz <mail+git@davidschwarz.eu>
2026-08-07 04:22:31 +0200

    add StatusResponce

---
+++ src/Http/StatusResponce.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of "genug".
+ *
+ * (c) David J. Schwarz
+ * https://davidschwarz.eu
+ *
+ * License: MIT License
+ */
+
+namespace genug\Http;
+
+use Stringable;
+
+final class StatusResponce implements Response
+{
+    public private(set) Status $status;
+
+    public private(set) ContentType $contentType;
+
+    public private(set) string|Stringable $body;
+
+    public function __construct(
+        Status $status,
+        ContentType $contentType = ContentType::HTML
+    ) {
+        $this->status = $status;
+        $this->contentType = $contentType;
+        $this->body = $status->description();
+    }
+}
