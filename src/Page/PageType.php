eb0db6153688d0d4e505b202d8a001157d6fe9ee
David J. Schwarz <mail+git@davidschwarz.eu>
2026-08-11 06:44:30 +0200

    add PageType

---
+++ src/Page/PageType.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of "genug".
+ *
+ * (c) David J. Schwarz
+ * https://davidschwarz.eu
+ *
+ * License: MIT License
+ */
+
+namespace genug\Page;
+
+use genug\Lib\Equatable;
+use genug\Lib\EquatableSimpleObjectTrait;
+
+enum PageType: string implements Equatable
+{
+    use EquatableSimpleObjectTrait;
+
+    case HTML = 'html';
+}
